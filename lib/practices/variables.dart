e58e9a6b7b32b47195367ccda3ddf0b04b2d9fe3
Muttakin Islam Hasib <muttakinislamhasib@gmail.com>
2024-08-30 00:00:36 +0600

    feat: 🛠️ variables and functions

---
+++ lib/practices/variables.dart
@@ -0,0 +1,19 @@
+String name = 'Muttakin Hasib';
+
+var otherName = 'Hello Hasib';
+
+var age;
+
+int? x;
+
+var nullValue;
+
+var zero = 0;
+
+if (zero == 0) {
+  print('Zero is zero');
+}
+
+bool fn() {
+  return true;
+}
